ccd7769503a220779bd390e8cf2047090f7f0e01
Adam Procter <adamprocter@researchnot.es>
2019-04-26 19:20:20 +0100

    tidy

--- src/store.js
+++ src/store.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import PouchDB from 'pouchdb'
-import { connectableObservableDescriptor } from 'rxjs/internal/observable/ConnectableObservable'
+
 Vue.use(Vuex)
 var pouchdb = new PouchDB('couchdocs')
 var remote = 'https://nn.adamprocter.co.uk/couchdocs'
@@ -24,7 +24,6 @@ const store = new Vuex.Store({
         })
     },
     ADD_DOC() {
-      console.log('add')
       var uniqueid =
         Math.random()
           .toString(36)
@@ -32,7 +31,6 @@ const store = new Vuex.Store({
         Math.random()
           .toString(36)
           .substring(2, 15)
-      // send added doc to the pouch
       pouchdb
         .get(mydoc)
         .then(function(doc) {
@@ -68,31 +66,29 @@ const store = new Vuex.Store({
   actions: {
     syncDB: () => {
       pouchdb.replicate.from(remote).on('complete', function() {
-        console.log('first sync completed')
-
         // turn on two-way, continuous, retriable sync
         pouchdb
           .sync(remote, { live: true, retry: true })
           .on('change', function(info) {
             // handle change
-            console.log('change')
+            //console.log('change')
             store.commit('GET_DB')
           })
           .on('paused', function() {
             // replication paused (e.g. replication up to date, user went offline)
-            console.log('replication paused')
+            // console.log('replication paused')
             // store.dispatch("get_data");
           })
           .on('active', function() {
             // replicate resumed (e.g. new changes replicating, user went back online)
-            console.log('back active')
+            //console.log('back active')
           })
           .on('denied', function() {
             // a document failed to replicate (e.g. due to permissions)
           })
           .on('complete', function() {
             // handle complete
-            console.log('complete')
+            //console.log('complete')
           })
           .on('error', function(err) {
             console.log(err)
