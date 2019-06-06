f0dd3bd3f17df6370a699f922bd3acd50ec3d5a9
Adam Procter <adamprocter@researchnot.es>
2019-06-06 19:34:29 +0100

    added todo list plug in
    started to use the TODO / FIXME syntax inside the project to better support issues across platforms and systems, rather than use other methods like Trello or other 3rd party options

--- src/components/SpaceView.vue
+++ src/components/SpaceView.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="spaceview">
     <h2>Space Man !</h2>
-    <!-- <ul class="data">
     <!-- tips-->
     <!-- : is short for v-bind -->
     <svg
@@ -9,7 +8,7 @@
       width="400"
       height="1800"
       id="space"
-      onload="makeDraggable(evt)"
+      @load="makeDraggable"
     >
       <g
         v-for="(note, index) in notes"
@@ -31,72 +30,74 @@ export default {
   name: 'YourData',
   computed: mapState({
     notes: state => state.notes
-  })
-}
-// drag code for now included in SpaceView
+  }),
 
-function makeDraggable(evt) {
-  console.log('yo')
-  var svg = evt.target
+  // TODO: Fix this method
+  methods: {
+    makeDraggable() {
+      console.log(evt)
+      var svg = evt.target
 
-  svg.addEventListener('mousedown', startDrag)
-  svg.addEventListener('mousemove', drag)
-  svg.addEventListener('mouseup', endDrag)
-  svg.addEventListener('mouseleave', endDrag)
-  svg.addEventListener('touchstart', startDrag)
-  svg.addEventListener('touchmove', drag)
-  svg.addEventListener('touchend', endDrag)
-  svg.addEventListener('touchleave', endDrag)
-  svg.addEventListener('touchcancel', endDrag)
+      svg.addEventListener('mousedown', startDrag)
+      svg.addEventListener('mousemove', drag)
+      svg.addEventListener('mouseup', endDrag)
+      svg.addEventListener('mouseleave', endDrag)
+      svg.addEventListener('touchstart', startDrag)
+      svg.addEventListener('touchmove', drag)
+      svg.addEventListener('touchend', endDrag)
+      svg.addEventListener('touchleave', endDrag)
+      svg.addEventListener('touchcancel', endDrag)
 
-  function getMousePosition(evt) {
-    var CTM = svg.getScreenCTM()
-    if (evt.touches) {
-      evt = evt.touches[0]
-    }
-    return {
-      x: (evt.clientX - CTM.e) / CTM.a,
-      y: (evt.clientY - CTM.f) / CTM.d
-    }
-  }
-
-  var selectedElement, offset, transform
-
-  function startDrag(evt) {
-    if (evt.target.classList.contains('draggable')) {
-      selectedElement = evt.target
-      offset = getMousePosition(evt)
-
-      // Make sure the first transform on the element is a translate transform
-      var transforms = selectedElement.transform.baseVal
-
-      if (
-        transforms.length === 0 ||
-        transforms.getItem(0).type !== SVGTransform.SVG_TRANSFORM_TRANSLATE
-      ) {
-        // Create an transform that translates by (0, 0)
-        var translate = svg.createSVGTransform()
-        translate.setTranslate(0, 0)
-        selectedElement.transform.baseVal.insertItemBefore(translate, 0)
+      function getMousePosition(evt) {
+        var CTM = svg.getScreenCTM()
+        if (evt.touches) {
+          evt = evt.touches[0]
+        }
+        return {
+          x: (evt.clientX - CTM.e) / CTM.a,
+          y: (evt.clientY - CTM.f) / CTM.d
+        }
       }
 
-      // Get initial translation
-      transform = transforms.getItem(0)
-      offset.x -= transform.matrix.e
-      offset.y -= transform.matrix.f
-    }
-  }
+      var selectedElement, offset, transform
 
-  function drag(evt) {
-    if (selectedElement) {
-      evt.preventDefault()
-      var coord = getMousePosition(evt)
-      transform.setTranslate(coord.x - offset.x, coord.y - offset.y)
-    }
-  }
+      function startDrag(evt) {
+        if (evt.target.classList.contains('draggable')) {
+          selectedElement = evt.target
+          offset = getMousePosition(evt)
 
-  function endDrag(evt) {
-    selectedElement = false
+          // Make sure the first transform on the element is a translate transform
+          var transforms = selectedElement.transform.baseVal
+
+          if (
+            transforms.length === 0 ||
+            transforms.getItem(0).type !== SVGTransform.SVG_TRANSFORM_TRANSLATE
+          ) {
+            // Create an transform that translates by (0, 0)
+            var translate = svg.createSVGTransform()
+            translate.setTranslate(0, 0)
+            selectedElement.transform.baseVal.insertItemBefore(translate, 0)
+          }
+
+          // Get initial translation
+          transform = transforms.getItem(0)
+          offset.x -= transform.matrix.e
+          offset.y -= transform.matrix.f
+        }
+      }
+
+      function drag(evt) {
+        if (selectedElement) {
+          evt.preventDefault()
+          var coord = getMousePosition(evt)
+          transform.setTranslate(coord.x - offset.x, coord.y - offset.y)
+        }
+      }
+
+      function endDrag(evt) {
+        selectedElement = false
+      }
+    }
   }
 }
 </script>
