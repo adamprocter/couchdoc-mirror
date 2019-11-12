921b49327f78e4c62b683b9c1608103bf4f12701
Adam Procter <adamprocter@researchnot.es>
2019-11-12 10:28:50 +0000

    adding reader view
    this is a new component to open up other peoples contributions alongside editor view

---
+++ src/components/ReaderView.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="readerview">
+    <h2>Reader View</h2>
+    <ul class="data">
+      <!-- tips -->
+      <!-- : is short for v-bind -->
+      <li v-for="(note, index) in notes" :key="index">{{ note.text }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+  name: 'ReaderView',
+  computed: mapState({
+    notes: state => state.notes
+  })
+}
+</script>
+
+<style lang="css" scoped></style>
