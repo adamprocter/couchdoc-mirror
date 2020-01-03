2beaecf1143ffb97c9a1f7896d5e6d72445be4fa
Adam Procter <adamprocter@researchnot.es>
2020-01-03 16:33:49 +0000

    updated to new onboarding process

--- src/components/OnBoard.vue
+++ src/components/OnBoard.vue
@@ -1,13 +1,5 @@
 <template>
   <div class="notlogged">
-    <h2>
-      instance name :
-      <b>{{ instance }}</b>
-    </h2>
-    <h2>
-      device name :
-      <b>{{ clientid }}</b>
-    </h2>
     <p>
       nodenoggin is a
       <span>work in progress</span> collaborative co-creation research and
@@ -17,15 +9,26 @@
       >about</a> section.
     </p>
 
+    <ul>
+      <li>
+        instance name :
+        <b>{{ localinstance }}</b>
+      </li>
+      <li>
+        device name :
+        <b>{{ clientid }}</b>
+      </li>
+    </ul>
+
     <form>
       <h2>1</h2>
       <h3>instance</h3>
       <p>create or join an instance. an instance is a sharable digital space that can be shared privately between a group of individuals. all content / data you contribute is stored locally on your device and then shared privately to others on the same instance. you can remove your contributions at any time, they belong to you.</p>
-      <input type="text" v-model="instance" placeholder="instance name" />
+      <input type="text" v-model="localinstance" placeholder="instance name" />
       <button @click="createInstance()">+</button>
     </form>
 
-    <form>
+    <form v-show="partb">
       <h2>2</h2>
       <h3>name</h3>
       <p>give yourself a unique name, this ties this device to your own content / data. this name is anonymous and stored privately.</p>
@@ -33,10 +36,10 @@
       <button @click="setClient()">+</button>
     </form>
 
-    <form>
+    <form v-show="partc">
       <h2>3</h2>
-      <h3>join</h3>
-      <button @click="setClient()">+</button>
+      <h3>start</h3>
+      <button @click="letsGo()">+</button>
     </form>
   </div>
 </template>
@@ -48,7 +51,9 @@ export default {
   data: function() {
     return {
       clientid: '',
-      instance: ''
+      localinstance: '',
+      partb: false,
+      partc: false
     }
   },
 
@@ -68,13 +73,17 @@ export default {
     setClient() {
       this.$store.dispatch('setClient', this.clientid),
         localStorage.setItem('myNNClient', this.clientid),
-        this.$emit('clientAdded')
+        (this.partc = true)
+    },
+    letsGo() {
+      this.$emit('clientAdded')
     },
     createInstance() {
-      this.$store.dispatch('createInstance', this.instance)
+      this.$store.dispatch('createInstance', this.localinstance),
+        (this.partb = true)
     },
     removeIndex() {
-      this.$store.dispatch('removeInstance', this.instance)
+      this.$store.dispatch('removeInstance', this.localinstance)
     }
   }
 }
@@ -90,11 +99,85 @@ html {
     font-family: 'Inter var', sans-serif;
   }
 }
+ul {
+  font-family: 'Pica 10 Pitch W01';
+  font-size: 16px;
+  line-height: 20px;
+}
+
+li:before {
+  content: '';
+}
+
+b {
+  background-color: yellow;
+  padding-right: 2em;
+}
 
 h1,
 h2,
 h3,
 p {
   font-family: 'Inter var', sans-serif;
+  color: black;
+  margin: 0px;
+}
+
+p {
+  font-family: chaparral-pro, serif;
+  font-weight: 400;
+  font-style: normal;
+  font-size: 16px;
+  margin-top: 2em;
+}
+
+h2 {
+  float: right;
+  font-size: 3em;
+}
+
+h3 {
+  font-size: 3em;
+  margin-top: 0.5em;
+}
+
+form {
+  padding: 1em;
+  border-style: solid;
+  border-width: 0.5em;
+  border-color: #cab6ff;
+  margin-top: 1em;
+}
+
+input {
+  border-style: solid;
+  border-width: 1px;
+  border-color: #cab6ff;
+  padding: 0.5em;
+}
+
+button {
+  font-size: 2em;
+  color: black;
+  border-style: solid;
+  border-width: 5px;
+  border-color: black;
+  border-radius: 5em;
+  background-color: white;
+  box-shadow: 0px 0px 0px #000000;
+  padding: 10px 18px;
+}
+
+button:active {
+  background-color: #cab6ff;
+}
+
+@media only screen and (min-width: 640px) {
+  /* Style adjustments for viewports that meet the condition */
+
+  .notlogged {
+    grid-column: 2 / 5;
+    grid-row: 1;
+  }
 }
 </style>
